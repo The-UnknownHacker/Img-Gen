dd5e0f18959bb470bad4eedd6bac114339d13988
CyberZen <128781393+The-UnknownHacker@users.noreply.github.com>
2024-05-10 16:34:42 +1000

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ import io
 
 app = Flask(__name__)
 
-API_URL = "https://frightened-dove-turtleneck-shirt.cyclic.app/proxy/https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-xl-base-1.0"
-HEADERS = {"Authorization": "Bearer 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"}
+
+API_URL = "https://frightened-dove-turtleneck-shirt.cyclic.app/proxy/https://api-inference.huggingface.co/models/prompthero/openjourney"
+headers = {"Authorization": "Bearer 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"}
 
 def query(payload):
     response = requests.post(API_URL, headers=HEADERS, json=payload)
